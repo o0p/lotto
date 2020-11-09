a907d1d19643176f6e8b54484562a59bc6579ec8
Russell <russelldev@protonmail.com>
2020-11-09 18:23:23 +0000

    Num picker

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 let maxNum = parseInt(prompt('enter max number'));
+while (!maxNum) {
+    maxNum = parseInt(prompt('enter a valid number'));
+}
 
 const targetNum = Math.floor(Math.random() * maxNum) + 1;
 
-console.log(targetNum)
+console.log(targetNum);
+
+let guess = parseInt(prompt('enter first guess'));
+while (guess !== targetNum) {
+    if (guess > targetNum) {
+        guess = parseInt(prompt('Too High! Try again:'));
+    } else {
+        guess = parseInt(prompt('Too Low! Try again:'));
+    }
+}
+console.log("you're winner!")
