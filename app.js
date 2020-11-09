a550836d7509f0a59d66f54ff4385d25dc6c42b1
Russell <russelldev@protonmail.com>
2020-11-09 18:02:14 +0000

    Num picker

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-// let maxNum = parseInt(prompt('enter max number'));
+let maxNum = parseInt(prompt('enter max number'));
 
-// const targenNum = Math.floor(Math.random() * maxNum) + 1;
-// const winningNum = [];
+const targetNum = Math.floor(Math.random() * maxNum) + 1;
 
-for (let i = 0, arr = []; i < 8; i ++){
-    arr[arr.length] = (Math.floor(Math.random() * 50) + 1);
-    let = winningNum = arr[i];
-    console.log(winningNum);
-}
+console.log(targetNum)
