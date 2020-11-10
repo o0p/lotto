3a0a4b4ce0a9f33be9e53a863b10c49b038990b4
Russell <russelldev@protonmail.com>
2020-11-10 15:53:39 +0000

    research branch

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let numRange = parseInt(prompt('How high?'));
-const randomizer = Math.floor(Math.random() * numRange) + 1;
+const numLucky = Math.floor(Math.random() * numRange) + 1;
+console.log(numLucky);
 while (!numRange){
     numRange = parseInt(prompt('Error'));
 }
 let = guess    = parseInt(prompt('Wassup homie?'));
 
-console.log(randomizer);
 
 
 
-while(parseInt(guess) !== randomizer) {
+while(parseInt(guess) !== numLucky) {
 
-    if (parseInt(guess) > randomizer) {
+    if (parseInt(guess) > numLucky) {
     guess = parseInt(prompt('Too damn high dawg!'));
 } else {
     guess = parseInt(prompt('Not high enough homie!'));
