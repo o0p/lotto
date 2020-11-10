8d1e913deb30cf5221b3190841e33abf86df70bc
Russell <russelldev@protonmail.com>
2020-11-10 14:16:07 +0000

    research branch

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
-let maxNum = parseInt(prompt('enter max number'));
-while (!maxNum) {
-    maxNum = parseInt(prompt('enter a valid number'));
-}
-
-const targetNum = Math.floor(Math.random() * maxNum) + 1;
-
-let guess = parseInt(prompt('enter first guess'));
-let attempts = 1;
-while (parseInt(guess) !== targetNum) {
-    if (guess === 'q') break;
-    attempts++;
-    if (guess > targetNum) {
-        guess = prompt('Too High! Try again:');
-    } else {
-        guess = prompt('Too Low! Try again:');
-    }
-}
-if(guess === 'q') {
-    console.log(`Quitting after ${attempts} attempts`)
-} else {
-    console.log(`You're winner! But it took you ${attempts} attempts`)
-}
+let numRange = parseInt(prompt('How high?'));
+console.log
