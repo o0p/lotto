c12caef1f5a4507c098777e72fcca8062b5a4718
Russell <russelldev@protonmail.com>
2020-11-10 15:29:10 +0000

    research branch

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-let numRange = parseInt(prompt('How high?'));
+let numRange = parseInt(prompt('How high?')),
+    guess    = parseInt(prompt('Wassup homie?'));
 
-while(!numRange){
+const randomizer = Math.floor(Math.random() * numRange) + 1;
+console.log(randomizer);
+
+
+while (!numRange){
     numRange = parseInt(prompt('Error'));
 }
 
-console.log(numRange);
+while(parseInt(guess) !== randomizer) {
+    
+    if( guess == NaN){
+        console.log('errRRRRRRR');
+        console.log(guess);
+
+    }
+    if (parseInt(guess) > randomizer) {
+    guess = prompt('Too damn high dawg!');
+} else {
+    guess = prompt('Not hight enough homie!');
+}
+}
